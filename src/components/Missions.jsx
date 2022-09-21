c7982cab428be22a9199d7c3325ce346209cae63
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-09-20 22:28:46 -0300

    8 requisito

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
+    const title = 'Missões';
     return (
       <div data-testid="missions">
+        <Title headline={ title } />
       </div>
     );
   }
