0670b131874b604f74f7fa546ced5bec3154daba
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-09-21 08:29:23 -0300

    10 requisito

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -15,4 +15,18 @@ class MissionCard extends Component {
   }
 }
 
+MissionCard.defaultProps = {
+  name: '',
+  year: '',
+  country: '',
+  destination: '',
+};
+
+MissionCard.propTypes = {
+  name: PropTypes.string,
+  year: PropTypes.string,
+  country: PropTypes.string,
+  destination: PropTypes.string,
+};
+
 export default MissionCard;
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
@@ -7,6 +9,9 @@ class Missions extends Component {
     return (
       <div data-testid="missions">
         <Title headline={ title } />
+        { missions.map(({ name: n, year: y, country: c, destination: d }) => (
+          <MissionCard key={ n } name={ n } year={ y } country={ c } destination={ d } />
+        ))}
       </div>
     );
   }
