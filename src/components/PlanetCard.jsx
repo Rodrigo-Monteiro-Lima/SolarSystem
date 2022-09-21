1077f805300550c1f2e81b7d4cf7bbd2c784020c
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-09-20 21:07:57 -0300

    5 requisito

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,7 +6,8 @@ class PlanetCard extends Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        {/* */}
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
